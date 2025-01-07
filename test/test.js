fafd748a572f420dfc47053e530a1d6037bfc80e
SaptatirthaChoudhury <saptatirtha@gmail.com>
2025-01-08 03:08:45 +0530

    for normal JS testing

---
+++ test/test.js
@@ -0,0 +1,3 @@
+let workoutObj = [{"date":"2025-01-05T20:37:08.002Z","id":"6109428002","coords":[24.92022343861128,92.86176681518556],"distance":11,"duration":11,"cadence":11,"type":"running","pace":1,"description":"Running on January 6"}]
+
+console.log(typeof workoutObj, workoutObj);
